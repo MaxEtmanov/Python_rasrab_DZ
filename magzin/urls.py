8db1fdcfe90dbe72ce4c5c624d313d017092fee9
MaxEtmanov <166302161+MaxEtmanov@users.noreply.github.com>
2025-03-30 18:04:25 +0400

    Delete magzin directory

--- magzin/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('', views.product_list, name='product_list'),
-    path('products/<int:product_id>/', views.product_detail, name='product_detail'),
-]
